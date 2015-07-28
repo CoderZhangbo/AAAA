9b920ed7180efb728087d41a3987904c18e19dc5
ZhangBo <zhangbo@zhangbodeAir.lan>
2015-07-28 22:36:12 +0800

    Initial Commit

---
+++ AAAA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAAA
+//
+//  Created by 张波 on 15/7/28.
+//  Copyright (c) 2015年 zhangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AAAA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AAAA
+//
+//  Created by 张波 on 15/7/28.
+//  Copyright (c) 2015年 zhangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
